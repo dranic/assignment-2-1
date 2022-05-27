8a8b2263338e674640655ee3fa04b8b364932f0c
Dranitsa_PD <pdranitsa@ibagroup.eu>
2022-05-27 10:51:51 +0300

    processing aborted records added

--- src/main/java/org/example/ap/Module.java
+++ src/main/java/org/example/ap/Module.java
@@ -3,7 +3,6 @@ package org.example.ap;
 import eu.ibagroup.easyrpa.engine.annotation.ApModuleEntry;
 import eu.ibagroup.easyrpa.engine.apflow.ApModule;
 import eu.ibagroup.easyrpa.engine.apflow.TaskOutput;
-import eu.ibagroup.easyrpa.engine.apflow.history.Execution;
 import eu.ibagroup.easyrpa.engine.boot.ApModuleRunner;
 import eu.ibagroup.easyrpa.engine.boot.configuration.DevelopmentConfigurationModule;
 import lombok.extern.slf4j.Slf4j;
@@ -11,11 +10,11 @@ import org.example.entity.ResourceDto;
 import org.example.tasks.CreateZipTask;
 import org.example.tasks.DownloadPageTask;
 import org.example.tasks.GetResourceContentTask;
+import org.example.tasks.ProccessAbortedRecordsTask;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
+import java.util.function.BiConsumer;
 
 @Slf4j
 @ApModuleEntry(name = "Assignment 1", description = "Assignment 1")
@@ -24,36 +23,39 @@ public class Module extends ApModule {
     @Override
     public TaskOutput run() throws Exception {
         getInput().set("input_file", "input.json");
-        CompletableFuture<TaskOutput> result = execute(getInput(), GetResourceContentTask.class);
+        TaskOutput result = execute(getInput(), GetResourceContentTask.class).get();
 
-        List<ResourceDto> resources = result.get().get("resources", List.class);
+        List<ResourceDto> resources = result.get("resources", List.class);
+
+        BiConsumer<TaskOutput, List<TaskOutput>> mergeStrategy =
+                (resOut, outputs) -> {
+                    List<ResourceDto> success = new ArrayList<>();
+                    List<ResourceDto> errors = new ArrayList<>();
+                    ResourceDto res;
+                    for (TaskOutput out : outputs) {
+                        res = out.get("resource", ResourceDto.class);
+                        if (res.isErrorOccurred()) {
+                            errors.add(res);
+                        } else {
+                            success.add(res);
+                        }
+                    }
+                    resOut.set("resources", success);
+                    resOut.set("errors", errors);
+                };
 
 
         TaskOutput mergedOutput = split(getInput(), resources, (inp, resource) ->{
             inp.set("resource", resource);
             return execute(inp, DownloadPageTask.class);
-        }).merge((finalOutput, taskOutputs) ->{
-            List<ResourceDto> processedResources = taskOutputs.stream()
-                    .map(output ->output.get("resource", ResourceDto.class))
-                    .collect((Collectors.toList()));
-            finalOutput.set("processed_resources", processedResources);
-        }).get();
-        List<ResourceDto> processedResources = mergedOutput.get("processed_resources",List.class);
-        processedResources.forEach(res -> {
-            log.info("############# " + res);
-        });
+        }).merge(mergeStrategy).get();
 
-        execute(getInput(), CreateZipTask.class);
-
-
-
-//        return execute(mergedOutput, ProcessResultsTask.class).get();
+        execute(mergedOutput, CreateZipTask.class);
+        execute(mergedOutput, ProccessAbortedRecordsTask.class);
 
         return doNotCareOfResult();
     }
-
         public static void main(String[] arg) {
-            //use this code to test your Automation Process
             ApModuleRunner.localLaunch(Module.class, new DevelopmentConfigurationModule());
         }
 }
--- src/main/java/org/example/tasks/ProccessAbortedRecordsTask.java
+++ src/main/java/org/example/tasks/ProccessAbortedRecordsTask.java
@@ -1,27 +1,29 @@
 package org.example.tasks;
 
+import java.nio.file.Files;
 import eu.ibagroup.easyrpa.engine.annotation.ApTaskEntry;
 import eu.ibagroup.easyrpa.engine.annotation.Input;
-import eu.ibagroup.easyrpa.engine.annotation.OnError;
-import eu.ibagroup.easyrpa.engine.annotation.Output;
 import eu.ibagroup.easyrpa.engine.apflow.ApTask;
-import eu.ibagroup.easyrpa.engine.exception.ErrorStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.example.entity.ResourceDto;
 
-import java.io.File;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @ApTaskEntry(name = "Parsing input.json file")
 @Slf4j
 public class ProccessAbortedRecordsTask extends ApTask {
-    @Input("resources")
-    private List<ResourceDto> resource;
+    @Input("errors")
+    private List<ResourceDto> resources;
 
     @Override
     public void execute() throws Exception {
-
+        List<String> fileContent = new ArrayList();
+        resources.forEach(resource -> {fileContent.add(resource.getName() + " - " + resource.getUrl());});
+        Path file = Paths.get("errors.txt");
+        Files.write(file, fileContent, StandardCharsets.UTF_8);
     }
 }
